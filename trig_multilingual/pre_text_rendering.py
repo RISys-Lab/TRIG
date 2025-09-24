bc8780414c5f13d787b4983e23e286cc700a8e19
fesvhtr <fesvhtr@gmail.com>
2025-09-24 05:41:18 +0400

    rename files for improved organization

--- trig_multilingual/pre_text_rendering.py
+++ trig_multilingual/pre_text_rendering.py
@@ -11,15 +11,15 @@ from datasets import load_dataset
 def get_args():
     parser = argparse.ArgumentParser()
     # parser.add_argument("--raw_path",default=r'H:\ProjectsPro\TRIG\dataset\raw_dataset\text-rendering', type=str)
-    parser.add_argument("--raw_path",default="/home/yanzhonghao/data/dataset_hub", type=str)
+    parser.add_argument("--raw_path",default=r"H:\ProjectsPro\TRIG\data\dataset_hub", type=str)
     parser.add_argument("--dataset", default="EasyText", type=str)
 
     return parser.parse_args()
 
 
 def load_data(args):
-    file_path = os.path.join(args.raw_path, args.dataset)
-    dataset = load_dataset(file_path)
+    os.environ["HF_TOKEN"] = "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"
+    dataset = load_dataset("lllrrnn/EasyText")
     
     subset = [
         item for item in dataset['train']
@@ -101,7 +101,7 @@ def send_request(messages, max_retries=5, delay=2):
     for attempt in range(max_retries):
         try:
             print("Sending request...")
-            api_key = 'AIzaSyBXIzzjc8yHr3oL4Gzz9R-QgAg5SbkbCwc'
+            api_key = 'AIzaSyBIWvi7ZY1arEcZzZkZLExSNun8z2c_8-k'
             client = OpenAI(api_key=api_key, base_url="https://generativelanguage.googleapis.com/v1beta/openai/")
             
             response = client.chat.completions.create(
@@ -203,7 +203,7 @@ async def async_process_single_item(client, idx, obj):
 
 
 async def async_process_data(data_list):
-    api_key = 'AIzaSyBXIzzjc8yHr3oL4Gzz9R-QgAg5SbkbCwc'
+    api_key = 'AIzaSyBIWvi7ZY1arEcZzZkZLExSNun8z2c_8-k'
     client = AsyncOpenAI(api_key=api_key, base_url="https://generativelanguage.googleapis.com/v1beta/openai/")
 
     tasks = [
